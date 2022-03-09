bb30c731173d05a8d1722141ae7c6c00455d0ba3
Edgmos <edgaras.mosiejus@gmail.com>
2022-03-10 00:15:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/textcounterapp/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.textcounterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.textcounterapp.utils.TextUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtResult;
+    EditText txtMain;
+    Spinner spOption;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.txtResult = findViewById(R.id.txtResult);
+        this.txtMain = findViewById(R.id.txtMain);
+        this.spOption = findViewById(R.id.spOption);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.counting_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spOption.setAdapter(adapter);
+    }
+
+    public void onBtnClickCalculate(View view) {
+        if(txtMain.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Text is empty", Toast.LENGTH_LONG).show();
+        }
+        else{
+
+            if (this.spOption.getSelectedItem().toString().equalsIgnoreCase("Words")) {
+                int wordsCount = TextUtils.getWordsCount(this.txtMain.getText().toString().split("\\s+"));
+                String wordsCountFormated = String.valueOf(wordsCount);
+                this.txtResult.setText(wordsCountFormated);
+
+
+            } else {
+                int charsCount = TextUtils.getCharsCount(this.txtMain.getText().toString());
+                String charsCountFormated = String.valueOf(charsCount);
+                this.txtResult.setText(charsCountFormated);
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/textcounterapp/utils/TextUtils.java
@@ -0,0 +1,10 @@
+package com.example.textcounterapp.utils;
+
+public class TextUtils {
+    public static int getCharsCount(String input){
+        return input.length();
+    }
+    public static int getWordsCount (String[] input){
+        return input.length;
+    }
+}
